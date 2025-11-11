bd3c21871e37741a922400272c46d264645c19d2
GuiSalles27 <guilherme.salles@alunos.ifsuldeminas.edu.br>
2025-11-11 19:52:07 -0300

    tentativa2

--- src/main/java/com/mycompany/hospital/dao/ConnectionFactory.java
+++ src/main/java/com/mycompany/hospital/dao/ConnectionFactory.java
@@ -14,10 +14,16 @@ import java.sql.SQLException;
  * Padrão Singlenton
  */
 public class ConnectionFactory {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/guilhermes_andressa_hospital?useSSL=false&serverTimezone=America/Sao_Paulo"; //A versão 8 do Heid possui criptografia 
+    /*private static final String DB_URL = "jdbc:mysql://localhost:3306/guilhermes_andressa_hospital?useSSL=false&serverTimezone=America/Sao_Paulo"; //A versão 8 do Heid possui criptografia 
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD= "";
+    private static final String DB_PASSWORD= "";*/
+    
+        //github / railway
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
     
     //variável estática que mantém a instância única de ConnectionFactory. Enqunato não se encerra uma intância não abre outra.
     private static ConnectionFactory instance;
